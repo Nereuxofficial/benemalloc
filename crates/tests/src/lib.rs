bb18f74a7b35988c844d67d3d749f2b938d8bc10
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-06-10 22:36:02 +0200

    feat: Working on generic benchmark implementation

--- crates/tests/src/lib.rs
+++ crates/tests/src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(panic_internals)]
 #![feature(fmt_internals)]
+#![feature(allocator_api)]
+
 #[cfg(test)]
 pub mod rust_tests;
 /* This should probably moved to another crate, since setting globalalloc influences rust_tests
--- crates/tests/src/rust_tests.rs
+++ crates/tests/src/rust_tests.rs
@@ -1,5 +1,7 @@
 use benemalloc::BeneAlloc;
-use std::alloc::GlobalAlloc;
+use rand::prelude::SliceRandom;
+use rand::Rng;
+use std::alloc::{Allocator, GlobalAlloc, Layout};
 use std::fmt::Arguments;
 
 #[test]
@@ -11,20 +13,33 @@ pub fn test_panic() {
 #[test]
 pub fn basic_alloc() {
     let mut allocator = BeneAlloc::new();
-    let layout = std::alloc::Layout::from_size_align(8, 8).unwrap();
+    let rng = &mut rand::thread_rng();
     let num = 1000;
     let mut allocations = vec![];
     for i in 0..num {
+        let num: u16 = rng.gen();
+        let num = num as usize;
+        let layout = std::alloc::Layout::from_size_align(num, num).unwrap();
         let ptr = unsafe { allocator.alloc(layout) };
         assert!(!ptr.is_null());
-        allocations.push(ptr);
+        allocations.push((ptr, layout));
     }
-    for _ in 0..num / 2 {
-        unsafe { allocator.dealloc(allocations.pop().unwrap(), layout) };
+    #[cfg(feature = "track_allocations")]
+    allocator.print();
+}
+
+fn check_can_access(allocations: &Vec<(*mut u8, Layout)>) {
+    for (ptr, layout) in allocations {
+        let mut sum = 0;
+        for i in 0..layout.size() {
+            unsafe {
+                sum += *ptr.add(i) as u8;
+            }
+        }
+        assert!(sum != 0);
     }
-    for _ in 0..num / 2 {
-        let ptr = unsafe { allocator.alloc(layout) };
-        assert!(!ptr.is_null());
-        allocations.push(ptr);
-    }
+}
+
+fn dealloc<A: Allocator + GlobalAlloc>(allocator: &mut A, allocation: (*mut u8, Layout)) {
+    unsafe { allocator.dealloc(allocation.0, allocation.1) };
 }
